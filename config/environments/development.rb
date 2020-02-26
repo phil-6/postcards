b1082a3dfeb0c10901885a445beab811b70ed9f6
phil-6 <github@purpleriver.dev>
2020-02-26 20:39:26 +0000

    configure emails to send via sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,19 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      user_name:      'apikey',
+      password:       ENV["SENDGRID_API_KEY"],
+      domain:         'postcards.purpleriver.dev',
+      address:        'smtp.sendgrid.net',
+      port:            '587',
+      authentication: :plain,
+      enable_starttls_auto: true
+  }
+
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
